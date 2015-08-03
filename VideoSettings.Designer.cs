df512b508fe3119cd275dcabd21e79d49cf4a84e
0xwas <was-0x@hotmail.co.jp>
2015-08-03 19:45:09 +0900

    Japanese Translation

--- VideoSettings.Designer.cs
+++ VideoSettings.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VideoSettings));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnSelectFile = new System.Windows.Forms.Button();
@@ -42,10 +43,7 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 82F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnSelectFile, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.txtVideoPath, 1, 0);
@@ -53,94 +51,54 @@
             this.tableLayoutPanel1.Controls.Add(this.txtOffset, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.trkHeightWidth, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.lblHeightWidth, 0, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 7);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 88);
-            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 8);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Video Path:";
             // 
             // btnSelectFile
             // 
-            this.btnSelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectFile.Location = new System.Drawing.Point(384, 3);
+            resources.ApplyResources(this.btnSelectFile, "btnSelectFile");
             this.btnSelectFile.Name = "btnSelectFile";
-            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
-            this.btnSelectFile.TabIndex = 1;
-            this.btnSelectFile.Text = "Browse...";
             this.btnSelectFile.UseVisualStyleBackColor = true;
             this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
             // 
             // txtVideoPath
             // 
-            this.txtVideoPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtVideoPath.Location = new System.Drawing.Point(85, 4);
+            resources.ApplyResources(this.txtVideoPath, "txtVideoPath");
             this.txtVideoPath.Name = "txtVideoPath";
-            this.txtVideoPath.Size = new System.Drawing.Size(293, 20);
-            this.txtVideoPath.TabIndex = 0;
             // 
             // label2
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 37);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Run Starts At:";
             // 
             // txtOffset
             // 
-            this.txtOffset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.txtOffset, "txtOffset");
             this.tableLayoutPanel1.SetColumnSpan(this.txtOffset, 2);
-            this.txtOffset.Location = new System.Drawing.Point(85, 33);
             this.txtOffset.Name = "txtOffset";
-            this.txtOffset.Size = new System.Drawing.Size(374, 20);
-            this.txtOffset.TabIndex = 2;
-            this.txtOffset.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // trkHeightWidth
             // 
-            this.trkHeightWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.trkHeightWidth, "trkHeightWidth");
             this.tableLayoutPanel1.SetColumnSpan(this.trkHeightWidth, 2);
-            this.trkHeightWidth.Location = new System.Drawing.Point(85, 61);
             this.trkHeightWidth.Name = "trkHeightWidth";
-            this.trkHeightWidth.Size = new System.Drawing.Size(374, 24);
-            this.trkHeightWidth.TabIndex = 3;
             this.trkHeightWidth.TickStyle = System.Windows.Forms.TickStyle.None;
             // 
             // lblHeightWidth
             // 
-            this.lblHeightWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblHeightWidth.AutoSize = true;
-            this.lblHeightWidth.Location = new System.Drawing.Point(3, 66);
+            resources.ApplyResources(this.lblHeightWidth, "lblHeightWidth");
             this.lblHeightWidth.Name = "lblHeightWidth";
-            this.lblHeightWidth.Size = new System.Drawing.Size(76, 13);
-            this.lblHeightWidth.TabIndex = 7;
-            this.lblHeightWidth.Text = "Height:";
             // 
             // VideoSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "VideoSettings";
-            this.Padding = new System.Windows.Forms.Padding(7);
-            this.Size = new System.Drawing.Size(476, 102);
             this.Load += new System.EventHandler(this.VideoSettings_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
