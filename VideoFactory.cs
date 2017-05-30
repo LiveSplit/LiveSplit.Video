21e058348ed95144750b2130949645be76ee5fb8
Sergey Papushin <spapushin@gmail.com>
2017-05-30 16:01:54 -0500

    Update version to 1.7

--- VideoFactory.cs
+++ VideoFactory.cs
@@ -20,6 +20,6 @@ namespace LiveSplit.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.6.3");
+        public Version Version => Version.Parse("1.7.0");
     }
 }
