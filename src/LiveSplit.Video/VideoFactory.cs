f98626c4e2bc7ba88a515dd3b1fe9dc7b725c507
wooferzfg <spapushin@gmail.com>
2024-05-11 09:48:38 -0400

    Update version to 1.8.29

--- src/LiveSplit.Video/VideoFactory.cs
+++ src/LiveSplit.Video/VideoFactory.cs
@@ -23,6 +23,6 @@ namespace LiveSplit.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.26");
+        public Version Version => Version.Parse("1.8.29");
     }
 }
